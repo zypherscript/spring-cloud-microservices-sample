c8a73f71cd49a1644989ff23c627e2c4cbc32db7
zypherscript <kitti.kthp@gmail.com>
2023-04-15 04:37:36 +0700

    configure sec for downstream

---
+++ downstream/src/main/java/com/example/downstream/config/SecurityConfiguration.java
@@ -0,0 +1,25 @@
+package com.example.downstream.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http
+        .csrf()
+        .disable()
+        .authorizeHttpRequests()
+        .anyRequest()
+        .authenticated()
+        .and()
+        .formLogin()
+        .and()
+        .httpBasic();
+    return http.build();
+  }
+}

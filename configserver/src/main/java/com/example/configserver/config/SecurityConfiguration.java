d2d5075cd387aa25a0b37e00dbcd4800621af06a
zypherscript <kitti.kthp@gmail.com>
2023-04-14 23:05:28 +0700

    up

---
+++ configserver/src/main/java/com/example/configserver/config/SecurityConfiguration.java
@@ -0,0 +1,21 @@
+package com.example.configserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.csrf()
+        .ignoringRequestMatchers("/encrypt/**", "/decrypt/**");
+    http.authorizeHttpRequests((requests) -> requests.anyRequest()
+        .authenticated());
+    http.formLogin();
+    http.httpBasic();
+    return http.build();
+  }
+}

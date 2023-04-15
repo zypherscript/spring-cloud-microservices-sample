dfae33823166c51f425fbfcaacabd2e94802cfb7
zypherscript <kitti.kthp@gmail.com>
2023-04-15 14:37:44 +0700

    pd & observation

--- downstream/src/main/java/com/example/downstream/DownstreamApplication.java
+++ downstream/src/main/java/com/example/downstream/DownstreamApplication.java
@@ -1,25 +1,40 @@
 package com.example.downstream;
 
 import com.netflix.discovery.EurekaClient;
+import io.micrometer.observation.Observation;
+import io.micrometer.observation.ObservationRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
+import org.springframework.http.ProblemDetail;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 @RefreshScope
-@RestController
 public class DownstreamApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(DownstreamApplication.class, args);
   }
+
+}
+
+@Controller
+@ResponseBody
+class RestController {
+
+  private final ObservationRegistry observationRegistry;
 
   @Autowired
   @Lazy
@@ -31,10 +46,28 @@ public class DownstreamApplication {
   @Value("${user.password:}")
   private String password;
 
-  @GetMapping(value = "/info/{username}", produces = MediaType.TEXT_PLAIN_VALUE)
-  public String getInfo(@PathVariable("username") String username) {
-    var from = eurekaClient.getApplication(appName).getName(); //proof with ProblemDetail
-    return "%s :: Hi %s, your pwd is %s".formatted(from, username, password);
+  RestController(ObservationRegistry observationRegistry) {
+    this.observationRegistry = observationRegistry;
   }
 
+  @GetMapping(value = "/info/{username}", produces = MediaType.TEXT_PLAIN_VALUE)
+  public String getInfo(@PathVariable("username") String username) {
+    var app = eurekaClient.getApplication(appName);
+    Assert.notNull(app, "application from eureka client mustn't be null");
+    return Observation
+        .createNotStarted("getInfo", this.observationRegistry)
+        .observe(() -> "%s :: Hi %s, your pwd is %s".formatted(app.getName(), username, password));
+  }
+}
+
+@ControllerAdvice
+class ErrorHandlingControllerAdvice {
+
+  @ExceptionHandler
+  ProblemDetail handleIllegalArgumentException(
+      IllegalArgumentException illegalArgumentException) {
+    var pd = ProblemDetail.forStatus(HttpStatusCode.valueOf(500));
+    pd.setDetail("application from eureka client is not ready yet :(");
+    return pd;
+  }
 }
